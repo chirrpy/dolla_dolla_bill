583278d184213e32f3f44174e42bc1954a32bfcc
Jeff Felchner <accounts+git@thekompanee.com>
2012-10-06 23:03:34 -0500

    First pass at making dolla dolla bill work it

--- lib/dolla_dolla_bill.rb
+++ lib/dolla_dolla_bill.rb
@@ -1,5 +1,37 @@
 require "dolla_dolla_bill/version"
+require 'money'
 
 module DollaDollaBill
-  # Your code goes here...
+  module ClassMethods
+    def money(name, options = {})
+      cents_field_reader     = options[:cents]    || :"#{name}_in_cents"
+      currency_field_reader  = options[:currency] || :"#{name}_currency"
+      cents_field_writer     = :"#{cents_field_reader}="
+      currency_field_writer  = :"#{currency_field_reader}="
+
+      define_method(name) do
+        return nil if send(cents_field_reader).nil? || send(currency_field_reader).nil?
+
+        ::Money.new(send(cents_field_reader), send(currency_field_reader))
+      end
+
+      define_method("#{name}=") do |value|
+        value = value.to_money
+
+        send(cents_field_writer,    value.cents)
+        send(currency_field_writer, value.currency_as_string)
+      end
+
+      self.class.send(:define_method, "lowest_#{name}") do
+        select("#{cents_field_reader}, #{currency_field_reader}").
+        order("#{cents_field_reader} ASC").
+        first.
+        price
+      end
+    end
+  end
+
+  def self.included(base)
+    base.extend ClassMethods
+  end
 end
---
+++ spec/dolla_dolla_bill_spec.rb
@@ -0,0 +1,102 @@
+require 'spec_helper'
+
+class ExoticDancer < ActiveRecord::Base
+  include DollaDollaBill
+
+  money :price
+end
+
+describe DollaDollaBill do
+  describe '#price' do
+    let(:money) do
+      ExoticDancer.new(
+        price_in_cents: cents,
+        price_currency: currency)
+    end
+
+    context 'when the money components are set' do
+      let(:cents)     { 1234  }
+      let(:currency)  { 'USD' }
+
+      it 'creates the proper Money object' do
+        money.price.cents.should              eql 1234
+        money.price.currency_as_string.should eql 'USD'
+      end
+    end
+
+    context 'when only the currency is set' do
+      let(:cents)     { nil   }
+      let(:currency)  { 'USD' }
+
+      it 'creates the proper Money object' do
+        money.price.should be_nil
+      end
+    end
+
+    context 'when only the cents are set' do
+      let(:cents)     { 1234 }
+      let(:currency)  { nil  }
+
+      it 'creates the proper Money object' do
+        money.price.should be_nil
+      end
+    end
+  end
+
+  describe '#price=' do
+    let(:money) { ExoticDancer.new }
+
+    context 'when set to a Money object' do
+      before { money.price = Money.new(5432, 'USD') }
+
+      it 'sets the attributes to the correct values' do
+        money.price_in_cents.should eql 5432
+        money.price_currency.should eql 'USD'
+      end
+    end
+
+    context 'when set to a string' do
+      before { money.price = '54.32' }
+
+      it 'sets the attributes to the correct values' do
+        money.price_in_cents.should eql 5432
+        money.price_currency.should eql 'USD'
+      end
+    end
+
+    context 'when set to a decimal' do
+      before { money.price = 54.32 }
+
+      it 'sets the attributes to the correct values' do
+        money.price_in_cents.should eql 5432
+        money.price_currency.should eql 'USD'
+      end
+    end
+  end
+
+  describe '.lowest_money' do
+    context 'when there are two items with different amounts' do
+      let!(:higher_money) { ExoticDancer.create( price_in_cents: 5433,
+                                                 price_currency: 'GBP') }
+
+      let!(:lower_money)  { ExoticDancer.create( price_in_cents: 5432,
+                                                 price_currency: 'GBP') }
+
+      it 'is the lower amount' do
+        ExoticDancer.lowest_price.should eql Money.new(5432, 'GBP')
+      end
+    end
+
+    context 'when there are two items with the same amounts' do
+      let!(:money_1) { ExoticDancer.create( price_in_cents: 5432,
+                                            price_currency: 'GBP') }
+
+      let!(:money_2) { ExoticDancer.create( price_in_cents: 5432,
+                                            price_currency: 'GBP') }
+
+      it 'is the correct amount' do
+        ExoticDancer.lowest_price.should eql Money.new(5432, 'GBP')
+      end
+    end
+  end
+end
